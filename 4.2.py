400644ccf30ed82205c233a69cf98d48d4be697f
Joanna <lewj@oregonstate.edu>
2017-09-24 19:19:40 -0700

    greedy pairs? 3/5

--- 4.2.py
+++ 4.2.py
@@ -1,107 +1,74 @@
-import fractions as frac
+from fractions import gcd
 
-
-def combinations(iterable, r):
-    # combinations('ABCD', 2) --> AB AC AD BC BD CD
-    # combinations(range(4), 3) --> 012 013 023 123
-    pool = tuple(iterable)
-    n = len(pool)
-    if r > n:
-        return
-    indices = range(r)
-    yield tuple(pool[i] for i in indices)
-    while True:
-        for i in reversed(range(r)):
-            if indices[i] != i + n - r:
-                break
-        else:
-            return
-        indices[i] += 1
-        for j in range(i+1, r):
-            indices[j] = indices[j-1] + 1
-        yield tuple(pool[i] for i in indices)
-
-
-# check if there's an infinite loop
-# return true if infinite loop available (good thing)
-# return false if numbers equal each other (bad thing)
-def check_inf(num1, num2, goodSet, badSet):
-    visited = set()
-    visited.add(frac.Fraction(num1, num2))
-
-    while num1 != num2:
-        if (frac.Fraction(num1, num2) in goodSet):
-            return True
-        if (frac.Fraction(num1, num2) in badSet):
-            return False
-
-        divide = frac.gcd(num1, num2)
-        if divide != 1:
-            num1 /= divide
-            num2 /= divide
-
-        if num1 < num2:
-            num1 += num1
-            num2 -= num1 / 2
-        else:
-            num2 += num2
-            num1 -= num2 / 2
-
-        if (frac.Fraction(num1, num2) in visited) or (frac.Fraction(num2, num1) in visited):
-            goodSet.add(frac.Fraction(num1, num2))
-            return True
-
-        visited.add(frac.Fraction(num1, num2))
-
-    badSet.add(frac.Fraction(num1, num2))
-    return False
-
-def isFour (num):
+def isBad (num):
     testnum = 4
     while testnum < num:
         testnum *= 2
     return testnum == num
 
-def better_check_inf(num1, num2):
+def check_inf(num1, num2):
     if num1 == num2:
         return False
 
-    divide = frac.gcd(num1, num2)
+    divide = gcd(num1, num2)
     num1 /= divide
     num2 /= divide
-    return not isFour(num1 + num2)
+    return not isBad(num1 + num2)
+
+def count_list(slist):
+    counts = dict()
+    for x in slist:
+        for y in x:
+            if y not in counts:
+                counts[y] = 1
+            else:
+                counts[y] += 1
+    return counts
+
+def elim_tuples1(slist, num):
+    lsize = len(slist)
+    tups = []
+
+    for i in range(lsize - 1, -1, -1):
+        if num in slist[i]:
+            if (slist[i][0] != num):
+                tups.append(slist[i][0])
+            else:
+                tups.append(slist[i][1])
+            del slist[i]
+
+    return tups
+
+def elim_tuples2(slist, num):
+    lsize = len(slist)
+    for i in range(lsize - 1, -1, -1):
+        if num in slist[i]:
+            del slist[i]
 
 def answer(banana_list):
-    # your code here
-    return
+    res_list = []                           # infinite banana guards
+    lsize = len(banana_list)                # number of guards
+    banana_list.sort()                      # maybe not necessary?
 
+    # put valid pairs in res_list
+    for i in range(lsize):
+        for j in range(i, lsize):
+            if (check_inf(banana_list[i], banana_list[j])):
+                res_list.append((banana_list[i], banana_list[j]))
 
+    # pair them up until no valid pairs
+    while (res_list):
+        counts = count_list(res_list)
+        mintup = min(counts, key=counts.get)
+        minparts = min(elim_tuples1(res_list, mintup))
+        elim_tuples2(res_list, minparts)
+        lsize -= 2
+    
+    return lsize
 
 
 #banana_list = [1, 1]
 banana_list = [1, 7, 3, 21, 13, 19]
 
-goodSet = set()
-goodSet.add(frac.Fraction(1,2))
-goodSet.add(frac.Fraction(1,4))
-goodSet.add(frac.Fraction(1,5))
-goodSet.add(frac.Fraction(1,6))
-goodSet.add(frac.Fraction(1,8))
-goodSet.add(frac.Fraction(1,9))
-goodSet.add(frac.Fraction(1,10))
-goodSet.add(frac.Fraction(1,11))
-goodSet.add(frac.Fraction(1,12))
-goodSet.add(frac.Fraction(1,13))
-goodSet.add(frac.Fraction(1,14))
+print answer(banana_list)
 
-badSet = set()
-badSet.add(frac.Fraction(1,3))
-badSet.add(frac.Fraction(1,7))
-badSet.add(frac.Fraction(1,15))
-#for x in combinations(banana_list, 2):
-#	print Fraction(x[0], x[1])
-
-test = 1
-
-for i in range(1, 101):
-    print "[%d]: " % i, check_inf(test, i, goodSet, badSet), better_check_inf(test, i)
